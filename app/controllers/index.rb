57dcc2618cc3aa6c703629df931e7a65683aed43
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-17 16:49:55 -0500

    ajaxified, attempt at making new page but twitter does not like us abusing their servers

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,13 +7,28 @@ get '/logout' do
   redirect '/'
 end
 
-get '/:username' do
+post '/:username' do
   @user = TwitterUser.find_or_create_by_username(params[:username])
   TwitterUser.store_tweets_if_stale(@user)
   @tweets = @user.fetch_tweets
-  erb :tweets
+  erb :tweets, :layout => false
 end
 
 post '/fetch' do
   redirect to "/#{params[:username]}"
+end
+
+get '/small_guy_finder/:username' do 
+  @friends = Twitter.friends(params[:username])
+  @small_guy_count = 9999999999
+  @small_guy = nil
+  @friends.each do |friend|
+    friend_count = Twitter.followers(friend.name).count
+    if friend_count < @small_guy_count
+      @small_guy_count = friend_count
+      @small_guy = friend
+    end
+  end
+
+  erb :small_guy
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.search-tweets').click(function(event){
+    event.preventDefault();
+    $('.tweets-container').html('<img src="img/gears.gif">');
+    user = $('input[name=username]').val();
+    $.post('/'+user, function(tweets){
+      $('.tweets-container').html(tweets);
+    })
+  })
 });
